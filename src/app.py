27f8aeca8a5b3c876df6c300ba00fe4078fe525b
Harrison Jin <harrison.jin@utexas.edu>
2022-10-16 12:06:45 -0500

    add debug sample data

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import json
 from typing import List
 from flask import Flask, request
 from flask_cors import CORS
@@ -6,15 +7,17 @@ import sys
 from task import Task, task_factory
 from task_scheduler import schedule_tasks
 
+DEBUG = 0
+
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/send_tasks', methods=['POST'])
 def generate_schedules() -> List[Task]:
-    print('ELLO', file=sys.stdout, flush=True)
-    print('asfdasdf', file=sys.stdout, flush=True)
+    if DEBUG:
+        with open('sample.txt', 'r') as f:
+            return json.load(f)
     r = request.get_json(force=True)
-    print(r, file=sys.stdout, flush=True)
 
     tasks_list = task_factory(r['tasks'], r['zip'])
     schedule = schedule_tasks(tasks_list)
