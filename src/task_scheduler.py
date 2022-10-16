3af2c1636d23e5654f4129d12b19eee2775ffc0e
jgbrown-ut <joshua.brown2@utexas.edu>
2022-10-16 05:47:49 -0500

    partial glue

--- src/task_scheduler.py
+++ src/task_scheduler.py
@@ -57,60 +57,53 @@ class time_slot:
 
 time_slots = []  # all tasks’ time slots
 
-weekday_offset_dict = {'Sun': 0,
-                       'Mon': 1,
-                       'Tue': 2,
-                       'Wed': 3,
-                       'Thu': 4,
-                       'Fri': 5,
-                       'Sat': 6}
+# weekday_offset_dict = {'Sun': 0,
+#                        'Mon': 1,
+#                        'Tue': 2,
+#                        'Wed': 3,
+#                        'Thu': 4,
+#                        'Fri': 5,
+#                        'Sat': 6}
 
 # loop thru tasks, create time_slots
 
 
-def get_time_slots(tasks1):
+def get_time_slots(tasks):
     time_slots = []
-    for task_tuple in tasks1:
-        task_name = task_tuple[0]
-        task_duration = task_tuple[1]
-        task_weekday_dict = task_tuple[2]
-        # print('\ntask_name:', task_name)
-        # print('task_duration:', task_duration)
-        # print('task_weekday_dict:', task_weekday_dict)
+    for task_class in tasks:
+        task_name = task_class.name
+        task_duration = task_class.duration.minutes
+        task_weekday_dict = task_class.allowed_times
 
         # weekdays and their time intervals
-        for day, day_intervals_array in task_weekday_dict.items():
+        for day_idx, day_intervals_array in task_weekday_dict.items():
             # all task intervals for the day
-            # print('day:', day)
-            # print('day_intervals_array:', day_intervals_array)
             for task_endpts in day_intervals_array:
-                weekday_offset = 24 * 60 * weekday_offset_dict[day]
+                weekday_offset = 24 * 60 * day_idx
                 start_military = task_endpts[0].hour * \
                     60 + task_endpts[0].minute
                 end_military = task_endpts[1].hour * 60 + task_endpts[1].minute
                 task_start = start_military + weekday_offset
                 task_end = end_military + weekday_offset
-                # print("stuff", task_start, task_end, task_duration)
                 # add all potential task durations
-                for start in range(task_start, task_end-task_duration+30, 30):
+                for start in range(task_start, task_end-task_duration+5, 5):
                     time_slots.append(
                         time_slot(start, start+task_duration, task_name))
-                    # print('appended')
     return time_slots
 
 
-time_slots = get_time_slots(tasks)
 # sort time slots
-
-
+time_slots = get_time_slots(tasks)
 time_slots = sorted(time_slots, key=lambda x: (x.end, x.start))
 
-print('all blocks')
-for slot in time_slots:
-    print(slot.task, ': ', slot.start, '-', slot.end)
 
-# def sort_function:
-#     # TODO
+def print_scheduled(tasks):
+    for task in tasks:
+        print(task.task, task.start, '-', task.end)
+
+
+print('all blocks')
+print_scheduled(time_slots)
 
 scheduled_tasks = []
 
@@ -135,20 +128,9 @@ def interval_schedule(time_slots):
     return task_counts
 
 
+# get counts of scheduled tasks
 task_counts = interval_schedule(time_slots)
-
 print('task_counts', task_counts, '\n')
-
-# print('scheduled blocks:')
-# for task in scheduled_tasks:
-#     print(task.task, task.start, '-', task.end)
-# print()
-
-
-def print_scheduled(tasks):
-    print('scheduled blocks:')
-    for task in tasks:
-        print(task.task, task.start, '-', task.end)
 
 
 unscheduled_tasks = tasks
@@ -156,7 +138,6 @@ for task in tasks:
     task_name = task[0]
     if task_name in task_counts:
         unscheduled_tasks.remove(task)
-
 print('unscheduled_tasks:', unscheduled_tasks)
 
 
@@ -167,61 +148,28 @@ for task_name, count in task_counts.items():
             if task.task == task_name:
                 duplicate_tasks.append(task)
 
-# print('duplicate_tasks', duplicate_tasks)
-# print('my dup')
-# print_scheduled(duplicate_tasks)
-
-
-def no_duplicates(task_counts):
-    for task_name, counts in task_counts.items():
-        if counts > 1:
-            return True
-
-    return False
-
-
-# print('no_duplicates', no_duplicates(task_counts))
 
 unscheduled_tasks_slots = get_time_slots(unscheduled_tasks)
 
-second_break = False
-final_task_reached = False
-one_pass = True
-while one_pass:
-    one_pass = False
-    # print('oh no dup', task_counts)
-    for dupe_task in duplicate_tasks:
-        for unscheduled_block in unscheduled_tasks_slots:
-            if unscheduled_block.start >= dupe_task.start and unscheduled_block.end <= dupe_task.end:
+# first pass - switch in unscheduled tasks
+for dupe_task in duplicate_tasks:
+    for unscheduled_block in unscheduled_tasks_slots:
+        if unscheduled_block.start >= dupe_task.start and unscheduled_block.end <= dupe_task.end:
+            scheduled_tasks.remove(dupe_task)
+            task_counts[dupe_task.task] -= 1
+            scheduled_tasks.append(unscheduled_block)
+            task_counts[unscheduled_block.task] = 1
+            break
 
-                scheduled_tasks.remove(dupe_task)
-                task_counts[dupe_task.task] -= 1
-                scheduled_tasks.append(unscheduled_block)
-                task_counts[unscheduled_block.task] = 1
-                # print('break')
-                break
-            if dupe_task == duplicate_tasks[-1]:
-                final_task_reached = True
-
-# print_scheduled(duplicate_tasks)
-
+# second pass - remove all duplicate tasks
 remove_tasks = []
 for task in scheduled_tasks:
     if task_counts[task.task] > 1:
         remove_tasks.append(task)
         task_counts[task.task] -= 1
-# print_scheduled(remove_tasks)
-
 for rm_task in remove_tasks:
     scheduled_tasks.remove(rm_task)
 
 
-print('scheduled blocks:')
-for task in scheduled_tasks:
-    print(task.task, task.start, '-', task.end)
-print()
-# print(task_counts)
-
-# times_slots = sorted(times_slots, key=itemgetter('end', 'start'))
-
-# interval scheduling
+print('final scheduled blocks:')
+print_scheduled(scheduled_tasks)
